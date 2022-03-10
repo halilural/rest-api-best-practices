e8c1c7cec6f0f0d44c9ad5888345ffee6bbf7b99
halilural <halilural5@gmail.com>
2022-03-10 22:25:50 +0300

    Initial commit

---
+++ src/test/java/com/uralhalil/restapibestpractices/RestApiBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uralhalil.restapibestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBestPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
